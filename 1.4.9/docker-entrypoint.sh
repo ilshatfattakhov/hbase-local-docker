9968eae0d88b3941701c7663e794c376474ba3a7
Ilshat Fattakhov <i.fattahov@gmail.com>
2019-06-21 21:35:56 +0300

    Adds hbase 1.4.9

---
+++ 1.4.9/docker-entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting hbase container with "$HBASE_ROLE" role"
+echo $ZOOKEEPER_QUORUM
+# Generate the config only if it doesn't exist
+if [ ! -f "conf/hbase-site.xml" ]; then
+    echo "conf/hbase-site.xml not found. creating new config file"
+    # touch "conf/hbase-site.xml"
+fi
+
+CONFIG="conf/hbase-site.xml"
+
+echo "<?xml version=\"1.0\"?>" > $CONFIG
+echo "<?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>" >> $CONFIG
+echo "<configuration>" >> $CONFIG
+echo "<property><name>hbase.rootdir</name><value>file://$HBASE_DATA_DIR</value></property>" >> $CONFIG
+echo "<property><name>hbase.cluster.distributed</name><value>true</value></property>" >> $CONFIG
+echo "<property><name>hbase.zookeeper.quorum</name><value>$ZOOKEEPER_QUORUM</value></property>" >> $CONFIG
+echo "<property><name>hbase.$HBASE_ROLE.hostname</name><value>$HOSTNAME</value></property>" >> $CONFIG
+echo "<property><name>hbase.$HBASE_ROLE.port</name><value>$PORT</value></property>" >> $CONFIG
+echo "</configuration>" >> $CONFIG
+
+cat $CONFIG
+
+# Generate the regionservers only if it doesn't exist
+if [ ! -f "conf/regionservers" ]; then
+    CONFIG="conf/regionservers"
+    echo $REGIONSERVERS | tr , '\n' >> $CONFIG
+fi
+
+echo "Running command "$@" & tail -f /dev/null"
+exec "$@"& tail -f /dev/null
